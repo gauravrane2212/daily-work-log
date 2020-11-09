483c22e2e4277269d0a5c2ced2ab14f74c6793ba
Gaurav Rane <gaurav.v.rane@gmail.com>
2020-11-08 18:45:44 -0800

    feat(server): adds more api endpoints
    Added API endpoints to filter based on sentiments and goals.
    Added API endpoints to delete and update existing logs

--- server/routes/logs.js
+++ server/routes/logs.js
@@ -8,6 +8,20 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+/* GET logs filtered by sentiment */
+router.route('/sentiment/:id').get((req, res) => {
+  Log.find({'sentiment': req.params.id})
+    .then(logs => res.json(logs))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+/* GET logs filtered by goal */
+router.route('/goal/:id').get((req, res) => {
+  Log.find({'goal': req.params.id})
+    .then(logs => res.json(logs))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 /* POST add a new log entry */
 router.route('/add').post((req, res) => {
   const date = Date.parse(req.body.date);
@@ -27,4 +41,26 @@ router.route('/add').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
+/* DELETE a specific log entry */
+router.route('/:id').delete((req, res) => {
+  Log.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Log entry deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+/* UPDATE a specific log entry */
+router.route('/update/:id').post((req, res) => {
+  Log.findById(req.params.id)
+    .then(log => {
+      log.date = Date.parse(req.body.date);
+      log.goal = req.body.goal;
+      log.description = req.body.description;
+      log.sentiment = req.body.sentiment;
+
+      log.save()
+        .then(() => res.json('Log entry updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
 module.exports = router;
